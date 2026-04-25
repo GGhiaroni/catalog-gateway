80fcf9cb3f278ee50bfa9acdea5fa0bcd710f9ae
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-25 12:25:10 -0300

    first commit: configuracao do docker-compose e do application.yaml

---
+++ src/main/java/com/GabrielTiziano/CatalogGateway/CatalogGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.GabrielTiziano.CatalogGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GabrielTiziano/CatalogGateway/CatalogGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GabrielTiziano.CatalogGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
