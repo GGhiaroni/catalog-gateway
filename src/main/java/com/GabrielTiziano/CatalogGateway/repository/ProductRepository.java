372a66471b4a66481d0405f7836305924657c173
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-27 15:43:01 -0300

    feat: Cria os Repository de Product e User

---
+++ src/main/java/com/GabrielTiziano/CatalogGateway/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.GabrielTiziano.CatalogGateway.repository;
+
+import com.GabrielTiziano.CatalogGateway.model.Product;
+import com.GabrielTiziano.CatalogGateway.model.enums.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    Page<Product> findByCategoryAndActiveTrue(Category category, Pageable pageable);
+    Page<Product> findByNameContainingIgnoreCaseAndActiveTrue(String name, Pageable pageable);
+    Page<Product> findByCategoryAndNameContainingIgnoreCaseAndActiveTrue(Category category, String name, Pageable pageable);
+    Page<Product> findByActiveTrue(Pageable pageable);
+}
