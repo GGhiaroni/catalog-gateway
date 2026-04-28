2a750bc0ec069d8f60f2c53809770df6059a7087
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-28 16:25:27 -0300

    feat: Cria os Mappers com os métodos responsáveis por converter DTO em Entity e vice-versa

---
+++ src/main/java/com/GabrielTiziano/CatalogGateway/mapper/ProductMapper.java
@@ -0,0 +1,48 @@
+package com.GabrielTiziano.CatalogGateway.mapper;
+
+import com.GabrielTiziano.CatalogGateway.dto.ProductRequest;
+import com.GabrielTiziano.CatalogGateway.dto.ProductResponse;
+import com.GabrielTiziano.CatalogGateway.model.Product;
+
+public class ProductMapper {
+    private ProductMapper() {
+    }
+
+    public static Product toEntity(ProductRequest productRequest) {
+        return Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .category(productRequest.category())
+                .price(productRequest.price())
+                .imageUrls(productRequest.imageUrls())
+                .attributes(productRequest.attributes())
+                .stock(productRequest.stock())
+                .build();
+    }
+
+    public static void updateEntity(ProductRequest productRequest, Product product) {
+                product.setName(productRequest.name());
+                product.setDescription(productRequest.description());
+                product.setCategory(productRequest.category());
+                product.setPrice(productRequest.price());
+                product.setImageUrls(productRequest.imageUrls());
+                product.setAttributes(productRequest.attributes());
+                product.setStock(productRequest.stock());
+    }
+
+    public static ProductResponse toResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .category(product.getCategory())
+                .price(product.getPrice())
+                .imageUrls(product.getImageUrls())
+                .attributes(product.getAttributes())
+                .stock(product.getStock())
+                .active(product.getActive())
+                .createdAt(product.getCreatedAt())
+                .updatedAt(product.getUpdatedAt())
+                .build();
+    }
+}
---
+++ src/main/java/com/GabrielTiziano/CatalogGateway/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package com.GabrielTiziano.CatalogGateway.mapper;
+
+import com.GabrielTiziano.CatalogGateway.dto.SignupRequest;
+import com.GabrielTiziano.CatalogGateway.dto.UserResponse;
+import com.GabrielTiziano.CatalogGateway.model.User;
+import com.GabrielTiziano.CatalogGateway.model.enums.Role;
+
+import java.util.stream.Collectors;
+
+public class UserMapper {
+    private UserMapper() {
+    }
+
+    public static User toEntity(SignupRequest signupRequest, String passwordHash) {
+        return User.builder()
+                .name(signupRequest.name())
+                .email(signupRequest.email())
+                .passwordHash(passwordHash)
+                .build();
+    }
+
+    public static UserResponse toResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .roles(user.getRoles().stream().map(Role::name).collect(Collectors.toSet()))
+                .createdAt(user.getCreatedAt())
+                .active(user.getActive())
+                .build();
+    }
+}
