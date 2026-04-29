80b9e457e148176da89cf1c008e5d6d4b1851de0
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-29 15:24:11 -0300

    fix: Adiciona a annotation @Repository no Product e UserRepository

--- src/main/java/com/GabrielTiziano/CatalogGateway/repository/ProductRepository.java
+++ src/main/java/com/GabrielTiziano/CatalogGateway/repository/ProductRepository.java
@@ -5,7 +5,9 @@ import com.GabrielTiziano.CatalogGateway.model.enums.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
     Page<Product> findByCategoryAndActiveTrue(Category category, Pageable pageable);
     Page<Product> findByNameContainingIgnoreCaseAndActiveTrue(String name, Pageable pageable);
