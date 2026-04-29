3b50b56b8fc59515bfcb10d4a8d519f7fae8f56f
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-29 00:07:44 -0300

    feat: Implementa os endpoints de autenticação no Controller e os seus respectivos Services

---
+++ src/main/java/com/GabrielTiziano/CatalogGateway/service/UserService.java
@@ -0,0 +1,35 @@
+package com.GabrielTiziano.CatalogGateway.service;
+
+import com.GabrielTiziano.CatalogGateway.dto.SignupRequest;
+import com.GabrielTiziano.CatalogGateway.dto.UserResponse;
+import com.GabrielTiziano.CatalogGateway.mapper.UserMapper;
+import com.GabrielTiziano.CatalogGateway.model.User;
+import com.GabrielTiziano.CatalogGateway.model.enums.Role;
+import com.GabrielTiziano.CatalogGateway.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserResponse register(SignupRequest signupRequest) {
+
+        if (userRepository.existsByEmail(signupRequest.email())) {
+            // TODO M3: trocar por EmailAlreadyExistsException
+            throw new RuntimeException("E-mail já cadastrado no banco de dados.");
+        }
+
+        String hashedPassword = passwordEncoder.encode(signupRequest.password());
+
+        User userEntity = UserMapper.toEntity(signupRequest, hashedPassword);
+        userEntity.setRoles(Set.of(Role.CUSTOMER));
+
+        return UserMapper.toResponse(userRepository.save(userEntity));
+    }
+}
