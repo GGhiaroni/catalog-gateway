3edf4dd380e57578bb1129b0f6353daa8ddca98b
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-03 09:22:39 -0300

    chore: Adiciona os rate limits por endpoint e roles no application.yaml e a annotation @EnableConfigurationProperties no arquivo principal da aplicacao

--- src/main/java/com/GabrielTiziano/CatalogGateway/CatalogGatewayApplication.java
+++ src/main/java/com/GabrielTiziano/CatalogGateway/CatalogGatewayApplication.java
@@ -2,8 +2,11 @@ package com.GabrielTiziano.CatalogGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.GabrielTiziano.CatalogGateway.config.RateLimitProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RateLimitProperties.class)
 public class CatalogGatewayApplication {
 
 	public static void main(String[] args) {
